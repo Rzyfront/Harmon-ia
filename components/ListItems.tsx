c8c91e835740972dd1acfca6f8542af157378024
rzyfront <rzyfront@gmail.com>
2023-06-02 18:03:35 -0500

    Start list songs

--- components/ListItems.tsx
+++ components/ListItems.tsx
@@ -1,8 +1,69 @@
-import React from 'react'
+'use client'
 
-const ListItems=() =>{
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import {FaPlay} from 'react-icons/fa'
+
+interface ListItemsProps {
+    image: string;
+    name: string;
+    href: string;
+}
+
+function ListItems({image, name, href}: ListItemsProps): JSX.Element {
+    const router = useRouter();
+    
+    const onClick = () => {
+        router.push(href);
+    }
+
   return (
-    <div>ListItems</div>
+    <button className="
+    relative
+    group
+    flex
+    items-center
+    rounded-md
+    overflow-hidden
+    gap-x-4
+    bg-neutral-100/10
+    hover:bg-neutral-100/20
+    transition
+    pr-4
+    ">
+        <div className="
+        relative
+        min-h-[64px]
+        min-w-[64px]
+        ">
+            <Image className="object-cover" fill src={image} alt="Imagen"/>
+        </div>
+        <p className="
+        font-medium
+        truncate
+        py-5
+        ">
+            {name}
+        </p>
+
+        <div className="
+        absolute
+        transition
+        opacity-0
+        rounded-full
+        flex
+        items-center
+        justify-center
+        bg-blue-600
+        p-4
+        drop-shadow-md
+        right-5
+        group-hover:opacity-100
+        hover:scale-110
+        ">
+            <FaPlay className="text-black"/>
+        </div>
+    </button>
   )
 }
 
